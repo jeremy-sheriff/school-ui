8ae20238b93776c30e24a80441b49aacc5276d0c
Jeremy <muhohoweb@icloud.com>
2024-09-12 10:04:42 +0300

    Build 1.0.5

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-version="1.0.4"
+version="1.0.5"
 docker build --platform linux/amd64 -t muhohoweb/school-ui:$version .
 
 docker push muhohoweb/school-ui:$version
 
-git add . && git commit -m "Build 1.0.1" && git push origin main
+git add . && git commit -m "Build 1.0.5" && git push origin main
--- src/app/components/students-component/students-component.component.ts
+++ src/app/components/students-component/students-component.component.ts
@@ -1,38 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import {StudentsServiceService} from "../../services/students-service.service";
-import {NgIf} from "@angular/common";
-import {NgFor} from "@angular/common";
- // Adjust the path to your service
+import { StudentsServiceService } from "../../services/students-service.service";
+import { NgIf } from "@angular/common";
+import { NgFor } from "@angular/common";
 
 @Component({
   selector: 'app-students-component',
   standalone: true,
-  imports: [
-    NgIf,NgFor
-  ],
+  imports: [NgIf, NgFor],
   templateUrl: './students-component.component.html',
   styleUrls: ['./students-component.component.css']
 })
 export class StudentsComponentComponent implements OnInit {
-  students: any[] = []; // Property to store the student data
-  errorMessage: string | null = null; // Property to store any error message
+  students: any[] = []; // Store student data
+  errorMessage: string | null = null; // Store any error message
+  loading: boolean = true; // Loading state to show/hide the loader
 
-  // Inject the service in the constructor
   constructor(private studentsService: StudentsServiceService) {}
 
-  // Implement OnInit lifecycle hook to call the service when the component is initialized
   ngOnInit(): void {
     this.fetchStudents();
   }
 
-  // Method to fetch students using the service
+  // Fetch students using the service
   fetchStudents() {
     this.studentsService.getStudents().subscribe({
       next: (data) => {
         this.students = data;
+        this.loading = false; // Hide loader when data is fetched
       },
       error: (err) => {
         this.errorMessage = 'Error fetching students';
+        this.loading = false; // Hide loader on error
         console.error(err);
       }
     });
