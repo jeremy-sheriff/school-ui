f183a0b64295c9341168edb56da2c347ab994cf3
Jeremy <muhohoweb@icloud.com>
2024-09-12 11:15:03 +0300

    Build 1.0.8

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-version="1.0.7"
+version="1.0.8"
 docker build --platform linux/amd64 -t muhohoweb/school-ui:$version .
 
 docker push muhohoweb/school-ui:$version
 
-git add . && git commit -m "Build 1.0.7" && git push origin main
+git add . && git commit -m "Build ${version}" && git push origin main
