cce0870a15b3c83f09d020aa092d8ca822094075
Jeremy <muhohoweb@icloud.com>
2024-09-23 08:01:16 +0300

    Build 2.0.4 with library and students component integrated

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-version="2.0.3"
+version="2.0.4"
 docker build --platform linux/amd64 -t muhohoweb/school-ui:$version .
 
 docker push muhohoweb/school-ui:$version
 
-git add . && git commit -m "Build ${version}" && git push origin main
+git add . && git commit -m "Build ${version} with library and students component integrated" && git push origin main
